7a724875fa4abf5210213fb0d88e6f65d840d324
leov1963 <leov1963@gmail.com>
2021-02-09 21:37:32 -0800

    Add Dino Model

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const express = require('express');
 const axios = require('axios');
 const ejsLayouts = require('express-ejs-layouts');
 const methodOverride = require('method-override');
+const db = require("./models")
 
 const app = express();
 app.set('view engine', 'ejs');
@@ -13,7 +14,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(methodOverride('_method'));
 
 app.get('/', (req, res) => {
-    res.send('Welcome to my App'); // Yo, Rome: what is this doing?
+    res.send('Welcome to my App'); 
 });
 
 const PORT = process.env.PORT || 8000;
